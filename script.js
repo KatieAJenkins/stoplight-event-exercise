5e9c6896e86a4850b7de2a6c7f1630080bd309b8
KatieAJenkins <kind2karma@gmail.com>
2017-05-15 20:44:49 -0600

    cleaned up code

--- script.js
+++ script.js
@@ -8,3 +8,33 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+stopButton.addEventListener('mouseover', function() {
+  console.log("Entered stopButton button");
+});
+
+stopButton.addEventListener('mouseout', function() {
+  console.log("Left stopButton button");
+});
+
+slowButton.addEventListener('mouseover', function() {
+  console.log("Entered slowButton button");
+});
+
+slowButton.addEventListener('mouseout', function() {
+  console.log("Left slowButton button");
+});
+
+goButton.addEventListener('mouseover', function() {
+  console.log("Entered goButton button");
+});
+
+goButton.addEventListener('mouseout', function() {
+  console.log("Left goButton button");
+});
+
+var logText = function (event) {
+  console.log(event.target.textContent);
+}
+
+controls.addEventListener('click', logText);
